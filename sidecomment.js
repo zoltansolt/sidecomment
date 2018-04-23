1ac3631637d66f12af26390284def0bc011f5a36
zoltansolt <zoltan.solt@gmail.com>
2018-04-23 22:58:50 +0200

    Add files via upload

--- sidecomment.js
+++ sidecomment.js
@@ -5,6 +5,13 @@ var counter = parseInt(localStorage.getItem('counter')) ? parseInt(localStorage.
 for (let i = 0; i < elems.length; i++) {
   renderButton(i);
 }
+let commentNodes = [];
+for (let i = 0; i < comments.length; i++) {
+  commentNodes[i] = renderComment(comments[i]);
+  if (!(comments[i].parentId == "p2" || comments[i].parentId == "p1" || comments[i].parentId == "p3")) {
+    commentNodes[i].className = "dialogbox-reply";
+  }
+}
 attachCommentsToParent();
 
 console.log(comments);
@@ -124,14 +131,24 @@ function resolveComment() {
   location.reload();
 }
 
-function attachCommentsToParent() {
-  let commentNodes = [];
-  for (let i = 0; i < comments.length; i++) {
-    commentNodes[i] = renderComment(comments[i]);
-    if (!(comments[i].parentId == "p2" || comments[i].parentId == "p1" || comments[i].parentId == "p3")) {
-      commentNodes[i].className = "dialogbox-reply";
+function hideComment(parent) {
+  for (let i = 0; i < commentNodes.length; i++) {
+    if (comments[i].parentId == parent) {
+      commentNodes[i].style.display = 'none';
     }
   }
+}
+
+function showComment(parent) {
+  for (let i = 0; i < commentNodes.length; i++) {
+    if (comments[i].parentId == parent) {
+      commentNodes[i].style.display = 'block';
+      showComment(comments[i].id);
+    }
+  }
+}
+
+function attachCommentsToParent() {
   for (let i = 0; i < allElems.length; i++) {
     for (let j = 0; j < comments.length; j++) {
       if (allElems[i].id === comments[j].parentId) {
